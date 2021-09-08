e46e07f5f23fa3b4277141c8dd4bc0ee1f350b8a
JCav <micojcav@outlook.com>
2021-09-09 06:50:27 +0800

    升级到6.2，添加音效

--- game/main.cpp
+++ game/main.cpp
@@ -5,6 +5,8 @@
 #include <QtCore>
 #include <QtWidgets>
 #include <QMediaPlayer>
+#include <QAudioOutput>
+#include <QSoundEffect>
 
 int main(int argc, char *argv[])
 {
@@ -23,11 +25,37 @@ int main(int argc, char *argv[])
 //    view.setFixedSize(view.size());
 //    view.show();
 
-    auto player = new QMediaPlayer;
-    connect(player, SIGNAL(positionChanged(qint64)), this, SLOT(positionChanged(qint64)));
-    player->setMedia(QUrl::fromLocalFile("/Users/me/Music/coolsong.mp3"));
-    player->setVolume(50);
-    player->play();
+//    QMediaPlayer player;
+//    QAudioOutput audioOutput; // chooses the default audio routing
+//    player.setAudioOutput(&audioOutput);
+//    player.setSource(QUrl("qrc:/asset/sound/background.m4a"));
+//    player.play();
+
+//    //player.setPosition(player.duration() / 2);
+
+//    QObject::connect(&player, &QMediaPlayer::durationChanged, [&player](qint64 v){
+//        qDebug()<<v;
+//        //player.setPosition(player.duration() / 2);
+//    });
+
+//    QObject::connect(&player, &QMediaPlayer::positionChanged, [&player](qint64 v){
+//        qDebug()<< "posi " <<v;
+//        //player.setPosition(player.duration() / 2);
+//    });
+
+
+//    QObject::connect(&player, &QMediaPlayer::mediaStatusChanged, [&player](QMediaPlayer::MediaStatus v){
+//        qDebug()<< "state " <<v;
+//        if(v == QMediaPlayer::MediaStatus::EndOfMedia);
+//        player.play();
+//        //player.setPosition(player.duration() / 2);
+//    });
+
+    QSoundEffect effect;
+    effect.setSource(QUrl("qrc:/asset/sound/click.wav"));
+    effect.setLoopCount(QSoundEffect::Infinite);
+    effect.setVolume(0.25f);
+    effect.play();
 
 
     return a.exec();
