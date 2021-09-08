59685d45dab7264059312db224f7b042e46a67de
JCav <micojcav@outlook.com>
2021-09-09 05:21:48 +0800

    添加音效

--- game/main.cpp
+++ game/main.cpp
@@ -1,13 +1,10 @@
 
 #include "R.h"
-#include "model/GameScene.h"
-#include "item/TextBoxItem.h"
-#include "ui/HUDDesign.h"
+#include "model/scene/HelloScene.h"
 #include <QApplication>
 #include <QtCore>
 #include <QtWidgets>
-
-#include "model/scene/HelloScene.h"
+#include <QMediaPlayer>
 
 int main(int argc, char *argv[])
 {
@@ -15,20 +12,23 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     R::initResource();
 
-    GameScene *game = new HelloScene();
-    game->init();
+//    GameScene *game = new HelloScene();
+//    game->init();
 
-    QGraphicsView view;
-    view.setScene(game);
-    view.setSceneRect({0, 0, 1024, 768});
-    view.show();
+//    QGraphicsView view;
+//    view.setScene(game);
+//    view.setSceneRect({0, 0, 1024, 768});
+//    view.show();
 
-    view.setFixedSize(view.size());
-    view.show();
+//    view.setFixedSize(view.size());
+//    view.show();
 
+    auto player = new QMediaPlayer;
+    connect(player, SIGNAL(positionChanged(qint64)), this, SLOT(positionChanged(qint64)));
+    player->setMedia(QUrl::fromLocalFile("/Users/me/Music/coolsong.mp3"));
+    player->setVolume(50);
+    player->play();
 
-//    HUDDesign h;
-//    h.show();
 
     return a.exec();
 }
